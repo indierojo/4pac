1967876004ce46a2c2624798bc3359eeabc74cbd
Jon Bullock <jon@talentriot.com>
2014-03-01 10:48:00 -0700

    Converted to SPA webapp template. Added Hello World in Canvas.

---
+++ 4PacCanvas/4PacCanvas/Startup.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(_4PacCanvas.Startup))]
+
+namespace _4PacCanvas
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
