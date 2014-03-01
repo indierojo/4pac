355fbbe36fd7ddbe60cd8e5e65b7fef9290e7244
Jon Bullock <jon@talentriot.com>
2014-03-01 14:01:21 -0700

    Restored index.html to its former hello world glory.

--- 4PacCanvas/4PacCanvas/app/canvas.ts
+++ 4PacCanvas/4PacCanvas/app/canvas.ts
@@ -1,3 +1,10 @@
 var canvas = <HTMLCanvasElement> document.getElementById('canvas');
 var readout = document.getElementById('readout');
 var context = canvas.getContext('2d');
+
+context.font = '38pt Arial';
+context.fillStyle = 'cornflowerblue';
+context.strokeStyle = 'blue';
+
+context.fillText('Hello World!', canvas.width/2 - 150, canvas.height/2 + 15);
+context.strokeText('Hello World!', canvas.width/2 - 150, canvas.height/2 + 15);
