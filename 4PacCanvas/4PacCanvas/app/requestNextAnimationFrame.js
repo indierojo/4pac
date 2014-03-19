4d3dfad6f226be2e2ce92b95896177f6cd707cfe
Jon Bullock <jondooger@gmail.com>
2014-03-19 17:05:23 -0600

    Add requestNextAnimationFrame shim.

---
+++ 4PacCanvas/4PacCanvas/app/requestNextAnimationFrame.js
@@ -0,0 +1,54 @@
+﻿var _this = this;
+window['requestNextAnimationFrame'] = (function () {
+    var originalWebkitMethod;
+    var wrapper;
+    var geckoVersion;
+    var userAgent = navigator.userAgent;
+    var index;
+    var self = _this;
+
+    // Chrome 10 fix:
+    if (window['webkitRequestAnimationFrame']) {
+        wrapper = function (time) {
+            if (time === undefined) {
+                time = +new Date();
+            }
+            self.callback(time);
+        };
+
+        originalWebkitMethod = window['webkitRequestAnimationFrame'];
+        window['webkitRequestAnimationFrame'] = function (cb, element) {
+            self.callback = cb;
+
+            originalWebkitMethod(wrapper, element);
+        };
+    }
+
+    // Firefox 4 fix:
+    if (window['mozRequestAnimationFrame']) {
+        index = userAgent.indexOf('rv:');
+
+        if (userAgent.indexOf('Gecko') != -1) {
+            geckoVersion = userAgent.substr(index + 3, 3);
+
+            if (geckoVersion === '2.0') {
+                window['mozRequestAnimationFrame'] = undefined;
+            }
+        }
+    }
+
+    // Shim fo browsers with none of the above:
+    var missingAnimationFrameShim = function (cb) {
+        var start, finish;
+        window.setTimeout(function () {
+            start = +new Date();
+            cb(start);
+            finish = +new Date();
+
+            self.timeout = 1000 / 60 - (finish - start);
+        }, self.timeout);
+    };
+
+    return window.requestAnimationFrame || window['webkitRequestAnimationFrame'] || window['mozRequestAnimationFrame'] || window.msRequestAnimationFrame || missingAnimationFrameShim;
+})();
+//# sourceMappingURL=requestNextAnimationFrame.js.map
---
+++ 4PacCanvas/4PacCanvas/app/requestNextAnimationFrame.ts
@@ -0,0 +1,56 @@
+﻿window['requestNextAnimationFrame'] = (()=> {
+    var originalWebkitMethod;
+    var wrapper;
+    var geckoVersion;
+    var userAgent = navigator.userAgent;
+    var index;
+    var self = this;
+
+    // Chrome 10 fix:
+    if (window['webkitRequestAnimationFrame']) {
+        wrapper = time=> {
+            if (time === undefined) {
+                time = +new Date();
+            }
+            self.callback(time);
+        };
+
+        originalWebkitMethod = window['webkitRequestAnimationFrame'];
+        window['webkitRequestAnimationFrame'] = (cb, element)=> {
+            self.callback = cb;
+
+            originalWebkitMethod(wrapper, element);
+        };
+    }
+
+    // Firefox 4 fix:
+    if (window['mozRequestAnimationFrame']) {
+        index = userAgent.indexOf('rv:');
+
+        if (userAgent.indexOf('Gecko') != -1) {
+            geckoVersion = userAgent.substr(index + 3, 3);
+
+            if (geckoVersion === '2.0') {
+                window['mozRequestAnimationFrame'] = undefined;
+            }
+        }
+    }
+
+    // Shim fo browsers with none of the above:
+    var missingAnimationFrameShim = (cb)=> {
+        var start, finish;
+        window.setTimeout(()=> {
+            start = +new Date();
+            cb(start);
+            finish = +new Date();
+
+            self.timeout = 1000 / 60 - (finish - start);
+        }, self.timeout);
+    };
+
+    return window.requestAnimationFrame ||
+        window['webkitRequestAnimationFrame'] ||
+        window['mozRequestAnimationFrame'] ||
+        window.msRequestAnimationFrame ||
+        missingAnimationFrameShim;
+})();
