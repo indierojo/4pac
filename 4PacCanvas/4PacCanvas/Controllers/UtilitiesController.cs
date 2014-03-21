5724518a920298bd4ce647305100badc68bb5c19
Jon Bullock <jondooger@gmail.com>
2014-03-20 21:00:38 -0600

    Made 4pac the index page of the home controller.

--- 4PacCanvas/4PacCanvas/Controllers/UtilitiesController.cs
+++ 4PacCanvas/4PacCanvas/Controllers/UtilitiesController.cs
@@ -2,35 +2,30 @@
 
 namespace _4PacCanvas.Controllers
 {
-    public class UtilitiesController : Controller
-    {
-        //
-        // GET: /Utilities/TestSpritesheet
-        public ActionResult TestSpritesheet()
-        {
-            return View(model: "../img/greenSheet.png");
-        }
-        //
-        // GET: /Utilities/BouncingBalls
-        public ActionResult BouncingBalls()
-        {
-            return View();
-        }
-        //
-        // GET: /Utilities/KeyboardMovement
-        public ActionResult KeyboardMovement()
-        {
-            return View();
-        }
+	public class UtilitiesController : Controller
+	{
+		//
+		// GET: /Utilities/TestSpritesheet
+		public ActionResult TestSpritesheet()
+		{
+			return View(model: "../img/greenSheet.png");
+		}
+		//
+		// GET: /Utilities/BouncingBalls
+		public ActionResult BouncingBalls()
+		{
+			return View();
+		}
+		//
+		// GET: /Utilities/KeyboardMovement
+		public ActionResult KeyboardMovement()
+		{
+			return View();
+		}
 
-        public ActionResult AnimationFrame()
-        {
-            return View();
-        }
-
-        public ActionResult FourPac()
-        {
-            return View();
-        }
+		public ActionResult AnimationFrame()
+		{
+			return View();
+		}
 	}
 }
