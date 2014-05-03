92df1fa62fb5f7b8ba8834ad355d5ea70f7c6b3b
Jon Bullock <jondooger@gmail.com>
2014-05-03 09:56:28 -0600

    Clear out a bunch of Auth artifacts from project.

--- 4PacCanvas/4PacCanvas/Startup.cs
+++ 4PacCanvas/4PacCanvas/Startup.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using Microsoft.Owin;
+﻿using Microsoft.Owin;
 using Owin;
 
 [assembly: OwinStartup(typeof(_4PacCanvas.Startup))]
@@ -12,7 +9,6 @@ namespace _4PacCanvas
     {
         public void Configuration(IAppBuilder app)
         {
-            ConfigureAuth(app);
         }
     }
 }
