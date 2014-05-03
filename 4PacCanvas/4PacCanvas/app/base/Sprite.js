13203a62da000038ce0175320f152f441279213e
Jon Bullock <jondooger@gmail.com>
2014-05-03 09:48:45 -0600

    Added sprite class.

---
+++ 4PacCanvas/4PacCanvas/app/base/Sprite.js
@@ -0,0 +1,22 @@
+﻿var Sprite = (function () {
+    function Sprite(name, painter, behaviors) {
+        this.name = name;
+        this.painter = painter;
+        this.behaviors = behaviors || [];
+        this.isAnimating = false;
+    }
+    Sprite.prototype.paint = function (context) {
+        if (this.painter !== undefined && this.isVisible) {
+            this.painter.paint(this, context);
+        }
+    };
+
+    Sprite.prototype.update = function (context, time) {
+        var _this = this;
+        this.behaviors.forEach(function (b) {
+            return b.execute(_this, context, time);
+        });
+    };
+    return Sprite;
+})();
+//# sourceMappingURL=Sprite.js.map
---
+++ 4PacCanvas/4PacCanvas/app/base/Sprite.ts
@@ -0,0 +1,29 @@
+﻿class Sprite {
+    name: string;
+    top: number;
+    left: number;
+    dimension: IDimension;
+    velocityX: number;
+    velocityY: number;
+    behaviors: Array<IBehavior>;
+    painter: IPainter;
+    isVisible: boolean;
+    isAnimating: boolean;
+
+    constructor(name: string, painter: IPainter, behaviors: Array<IBehavior>) {
+        this.name = name;
+        this.painter = painter;
+        this.behaviors = behaviors || [];
+        this.isAnimating = false;
+    }
+
+    paint(context: CanvasRenderingContext2D) {
+        if (this.painter !== undefined && this.isVisible) {
+            this.painter.paint(this, context);
+        }
+    }
+
+    update(context: CanvasRenderingContext2D, time: number) {
+        this.behaviors.forEach(b => b.execute(this, context, time));
+    }
+}
