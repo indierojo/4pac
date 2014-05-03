d7959ddaef32801f36a295f3d2dcda47abf621ec
Jon Bullock <jondooger@gmail.com>
2014-05-03 11:42:12 -0600

    Add Image Painter utility.

---
+++ 4PacCanvas/4PacCanvas/app/lib/ImagePainter.js
@@ -0,0 +1,13 @@
+﻿var ImagePainter = (function () {
+    function ImagePainter(imageUrl) {
+        this.image = new Image();
+        this.imageUrl = imageUrl;
+    }
+    ImagePainter.prototype.paint = function (sprite, context) {
+        if (this.image.complete) {
+            context.drawImage(this.image, sprite.left, sprite.top, sprite.dimension.width, sprite.dimension.height);
+        }
+    };
+    return ImagePainter;
+})();
+//# sourceMappingURL=ImagePainter.js.map
---
+++ 4PacCanvas/4PacCanvas/app/lib/ImagePainter.ts
@@ -0,0 +1,15 @@
+﻿class ImagePainter implements IPainter {
+    image: HTMLImageElement;
+    imageUrl: string;
+
+    constructor(imageUrl: string) {
+        this.image = new Image();
+        this.imageUrl = imageUrl;
+    }
+
+    paint(sprite: Sprite, context: CanvasRenderingContext2D) {
+        if (this.image.complete) {
+            context.drawImage(this.image, sprite.left, sprite.top, sprite.dimension.width, sprite.dimension.height);
+        }
+    }
+}
