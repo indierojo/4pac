48900be99e542a75436d3867d3980a277d6e58b9
Jon Bullock <jondooger@gmail.com>
2017-01-29 19:53:10 -0700

    Convert keyboard movement to es6 class.

--- src/utils/keyboardMovement.ts
+++ src/utils/keyboardMovement.ts
@@ -1,127 +1,139 @@
-const keyboardMovementCanvas = <HTMLCanvasElement> document.getElementById("canvas");
-const keyboardMovementDrawingContext = keyboardMovementCanvas.getContext("2d");
+export default class KeyboardMovement {
+    private canvas: HTMLCanvasElement;
+    private drawingContext: CanvasRenderingContext2D;
 
-const upLabel = document.getElementById("upLabel");
-const downLabel = document.getElementById("downLabel");
-const leftLabel = document.getElementById("leftLabel");
-const rightLabel = document.getElementById("rightLabel");
+    private upLabel: HTMLElement;
+    private downLabel: HTMLElement;
+    private leftLabel: HTMLElement;
+    private rightLabel: HTMLElement;
 
-const upArrowKeycode = 38;
-const downArrowKeycode = 40;
-const leftArrowKeycode = 37;
-const rightArrowKeycode = 39;
+    private upArrowKeycode = 38;
+    private downArrowKeycode = 40;
+    private leftArrowKeycode = 37;
+    private rightArrowKeycode = 39;
 
-let upPressed: boolean = false;
-let downPressed: boolean = false;
-let leftPressed: boolean = false;
-let rightPressed: boolean = false;
+    private upPressed = false;
+    private downPressed = false;
+    private leftPressed = false;
+    private rightPressed = false;
 
-const circle = {
-    x: 100,
-    y: 100,
-    radius: 15,
-    color: "#FFFF77"
-};
-
-drawCircle();
-move();
-
-keyboardMovementDrawingContext.lineWidth = 0.5;
-keyboardMovementDrawingContext.font = "32pt Arial";
-
-window.onkeydown = e => {
-    const keyCode = e.keyCode;
-
-    if (keyCode === upArrowKeycode) {
-        upPressed = true;
-        downPressed = false;
-    }
-    if (keyCode === downArrowKeycode) {
-        upPressed = false;
-        downPressed = true;
-    }
-    if (keyCode === leftArrowKeycode) {
-        leftPressed = true;
-        rightPressed = false;
-    }
-    if (keyCode === rightArrowKeycode) {
-        leftPressed = false;
-        rightPressed = true;
+    private circle = {
+        x: 100,
+        y: 100,
+        radius: 15,
+        color: "#FFFF77"
     }
 
-    move();
-    updateLabels();
-};
+    constructor() {
+        this.canvas = <HTMLCanvasElement> document.getElementById("canvas");
+        this.drawingContext = this.canvas.getContext("2d");
 
-window.onkeyup = e => {
-    const keyCode = e.keyCode;
-    if (keyCode === upArrowKeycode) {
-        upPressed = false;
-    }
-    if (keyCode === downArrowKeycode) {
-        downPressed = false;
-    }
-    if (keyCode === leftArrowKeycode) {
-        leftPressed = false;
-    }
-    if (keyCode === rightArrowKeycode) {
-        rightPressed = false;
-    }
+        this.upLabel = document.getElementById("upLabel");
+        this.downLabel = document.getElementById("downLabel");
+        this.leftLabel = document.getElementById("leftLabel");
+        this.rightLabel = document.getElementById("rightLabel");
 
-    updateLabels();
-    move();
-};
+        this.drawCircle();
+        this.move();
 
-function updateLabels() {
-    const upclassName = upPressed ? "green" : "red";
-    upLabel.setAttribute("class", upclassName);
-    downLabel.className = downPressed ? "green" : "red";
-    leftLabel.className = leftPressed ? "green" : "red";
-    rightLabel.className = rightPressed ? "green" : "red";
-}
+        this.drawingContext.lineWidth = 0.5;
+        this.drawingContext.font = "32pt Arial";
 
-function drawCircle() {
-    keyboardMovementDrawingContext.beginPath();
-    keyboardMovementDrawingContext.fillStyle = circle.color;
-    keyboardMovementDrawingContext.arc(circle.x, circle.y, circle.radius, 0, 2 * Math.PI);
-    keyboardMovementDrawingContext.fill();
-}
+        window.onkeydown = e => {
+            const keyCode = e.keyCode;
 
-function move() {
+            if (keyCode === this.upArrowKeycode) {
+                this.upPressed = true;
+                this.downPressed = false;
+            }
+            if (keyCode === this.downArrowKeycode) {
+                this.upPressed = false;
+                this.downPressed = true;
+            }
+            if (keyCode === this.leftArrowKeycode) {
+                this.leftPressed = true;
+                this.rightPressed = false;
+            }
+            if (keyCode === this.rightArrowKeycode) {
+                this.leftPressed = false;
+                this.rightPressed = true;
+            }
 
-    let stepX = 0;
-    let stepY = 0;
+            this.move();
+            this.updateLabels();
+        };
 
-    if (leftPressed) {
-        stepX = -10;
-    }
-    if (upPressed) {
-        stepY = -10;
-    }
-    if (downPressed) {
-        stepY = 10;
-    }
-    if (rightPressed) {
-        stepX = 10;
-    }
+        window.onkeyup = e => {
+            const keyCode = e.keyCode;
+            if (keyCode === this.upArrowKeycode) {
+                this.upPressed = false;
+            }
+            if (keyCode === this.downArrowKeycode) {
+                this.downPressed = false;
+            }
+            if (keyCode === this.leftArrowKeycode) {
+                this.leftPressed = false;
+            }
+            if (keyCode === this.rightArrowKeycode) {
+                this.rightPressed = false;
+            }
 
-    if (stepX === 0 && stepY === 0) {
-        return;
-    }
-
-    keyboardMovementDrawingContext.clearRect(0, 0, keyboardMovementDrawingContext.canvas.width, keyboardMovementDrawingContext.canvas.height);
-
-    const newX = circle.x + stepX;
-    const newY = circle.y + stepY;
-
-    console.log("ballX: " + newX + ", ballY: " + newY);
-
-    if (newX < keyboardMovementDrawingContext.canvas.width && newX > 0) {
-        circle.x = newX;
-    }
-    if (newY < keyboardMovementDrawingContext.canvas.height && newY > 0) {
-        circle.y = newY;
+            this.updateLabels();
+            this.move();
+        };
     }
 
-    drawCircle();
+    private updateLabels = () => {
+        const upclassName = this.upPressed ? "green" : "red";
+        this.upLabel.setAttribute("class", upclassName);
+        this.downLabel.className = this.downPressed ? "green" : "red";
+        this.leftLabel.className = this.leftPressed ? "green" : "red";
+        this.rightLabel.className = this.rightPressed ? "green" : "red";
+    }
+
+    private drawCircle = () => {
+        this.drawingContext.beginPath();
+        this.drawingContext.fillStyle = this.circle.color;
+        this.drawingContext.arc(this.circle.x, this.circle.y, this.circle.radius, 0, 2 * Math.PI);
+        this.drawingContext.fill();
+    }
+
+    private move = () => {
+
+        let stepX = 0;
+        let stepY = 0;
+
+        if (this.leftPressed) {
+            stepX = -10;
+        }
+        if (this.upPressed) {
+            stepY = -10;
+        }
+        if (this.downPressed) {
+            stepY = 10;
+        }
+        if (this.rightPressed) {
+            stepX = 10;
+        }
+
+        if (stepX === 0 && stepY === 0) {
+            return;
+        }
+
+        this.drawingContext.clearRect(0, 0, this.drawingContext.canvas.width, this.drawingContext.canvas.height);
+
+        const newX = this.circle.x + stepX;
+        const newY = this.circle.y + stepY;
+
+        console.log("ballX: " + newX + ", ballY: " + newY);
+
+        if (newX < this.drawingContext.canvas.width && newX > 0) {
+            this.circle.x = newX;
+        }
+        if (newY < this.drawingContext.canvas.height && newY > 0) {
+            this.circle.y = newY;
+        }
+
+        this.drawCircle();
+    }
 }
