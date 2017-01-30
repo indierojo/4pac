cb144eb324b007e857d02e0c9c87d095da0411eb
Jon Bullock <jondooger@gmail.com>
2017-01-29 19:44:14 -0700

    Convert balls.ts to an es6 class.

--- src/utils/balls.ts
+++ src/utils/balls.ts
@@ -9,74 +9,86 @@ interface ICircle {
     color: string;
 }
 
-const ballsCanvas = <HTMLCanvasElement> document.getElementById("canvas");
-const ballsDrawingContext = ballsCanvas.getContext("2d");
-const startButton = document.getElementById("startButton");
-const glassPane = document.getElementById("glasspane");
-let ballsPaused = true;
-const circles = [];
+export default class BouncingBalls {
+    private canvas: HTMLCanvasElement;
+    private drawingContext: CanvasRenderingContext2D;
+    private startButton: HTMLElement;
+    private glassPane: HTMLElement;
+    private paused: boolean;
+    private circles: ICircle[];
 
-drawGrid(ballsDrawingContext, "lightgray", 10, 10);
+    constructor() {
+        this.canvas = <HTMLCanvasElement> document.getElementById("canvas");
+        this.drawingContext = this.canvas.getContext("2d");
+        this.startButton = document.getElementById("startButton");
+        this.glassPane = document.getElementById("glasspane");
+        this.paused = true;
+        this.circles = [];
 
-ballsDrawingContext.lineWidth = 0.5;
-ballsDrawingContext.font = "32pt Arial";
+        drawGrid(this.drawingContext, "lightgray", 10, 10);
 
-for (let i = 1; i < 100; i++) {
-    circles[i] = {
-        x: 100,
-        y: 100,
-        velocityX: 3 * Math.random(),
-        velocityY: 3 * Math.random(),
-        radius: 50 * Math.random(),
-        color: "rgba(" + (Math.random() * 255).toFixed(0) + ", " +
-        (Math.random() * 255).toFixed(0) + ", " +
-        (Math.random() * 255).toFixed(0) + ", 1.0)"
-    };
-}
+        this.drawingContext.lineWidth = 0.5;
+        this.drawingContext.font = "32pt Arial";
 
-startButton.onclick = e => {
-    e.preventDefault();
-    e.stopPropagation();
-    ballsPaused = !ballsPaused;
-    startButton.innerText = ballsPaused ? "Start" : "Stop";
-};
+        for (let i = 1; i < 100; i++) {
+            this.circles[i] = {
+                x: 100,
+                y: 100,
+                velocityX: 3 * Math.random(),
+                velocityY: 3 * Math.random(),
+                radius: 50 * Math.random(),
+                color: "rgba(" + (Math.random() * 255).toFixed(0) + ", " +
+                (Math.random() * 255).toFixed(0) + ", " +
+                (Math.random() * 255).toFixed(0) + ", 1.0)"
+            };
+        }
 
-glassPane.onmousedown = e => {
-    e.preventDefault();
-    e.stopPropagation();
-};
+        this.startButton.onclick = e => {
+            e.preventDefault();
+            e.stopPropagation();
+            this.paused = !this.paused;
+            this.startButton.innerText = this.paused ? "Start" : "Stop";
+        };
 
-ballsDrawingContext.canvas.onmousedown = e => {
-    e.preventDefault();
-    e.stopPropagation();
-};
+        this.glassPane.onmousedown = e => {
+            e.preventDefault();
+            e.stopPropagation();
+        };
 
-setInterval(() => {
-    if (!ballsPaused) {
-        ballsDrawingContext.clearRect(0, 0, ballsDrawingContext.canvas.width, ballsDrawingContext.canvas.height);
-        drawGrid(ballsDrawingContext, "lightgray", 10, 10);
+        this.drawingContext.canvas.onmousedown = e => {
+            e.preventDefault();
+            e.stopPropagation();
+        };
 
-        circles.forEach(circle => {
-            ballsDrawingContext.beginPath();
-            ballsDrawingContext.arc(circle.x, circle.y, circle.radius, 0, Math.PI * 2, false);
-            ballsDrawingContext.fillStyle = circle.color;
-            ballsDrawingContext.fill();
-            adjustPosition(circle);
-        });
+        setInterval(() => {
+            if (!this.paused) {
+                this.drawingContext.clearRect(0, 0, this.drawingContext.canvas.width, this.drawingContext.canvas.height);
+                drawGrid(this.drawingContext, "lightgray", 10, 10);
+
+                this.circles.forEach(circle => {
+                    this.drawingContext.beginPath();
+                    this.drawingContext.arc(circle.x, circle.y, circle.radius, 0, Math.PI * 2, false);
+                    this.drawingContext.fillStyle = circle.color;
+                    this.drawingContext.fill();
+                    this.adjustPosition(circle);
+                });
+            }
+        }, 1000 / 60);
+
     }
-}, 1000 / 60);
 
-function adjustPosition(circle: ICircle) {
-    if (circle.x + circle.velocityX + circle.radius > ballsDrawingContext.canvas.width ||
-        circle.x + circle.velocityX - circle.radius < 0) {
-        circle.velocityX = -circle.velocityX;
+    private adjustPosition = (circle: ICircle) => {
+        if (circle.x + circle.velocityX + circle.radius > this.drawingContext.canvas.width ||
+            circle.x + circle.velocityX - circle.radius < 0) {
+            circle.velocityX = -circle.velocityX;
+        }
+
+        if (circle.y + circle.velocityY + circle.radius > this.drawingContext.canvas.height ||
+            circle.y + circle.velocityY - circle.radius < 0) {
+            circle.velocityY = -circle.velocityY;
+        }
+
+        circle.x += circle.velocityX;
+        circle.y += circle.velocityY;
     }
-
-    if (circle.y + circle.velocityY + circle.radius > ballsDrawingContext.canvas.height ||
-        circle.y + circle.velocityY - circle.radius < 0) {
-        circle.velocityY = -circle.velocityY;
-    }
-
-    circle.x += circle.velocityX;
-    circle.y += circle.velocityY;
 }
