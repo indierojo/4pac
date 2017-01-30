111815c737e4479cd1d095c0a70ed0d948938e67
Jon Bullock <jondooger@gmail.com>
2017-01-29 19:58:22 -0700

    Convert sprites to use es6 classes.

--- src/utils/sprites.ts
+++ src/utils/sprites.ts
@@ -2,45 +2,52 @@
 
 import { drawGrid } from "../engine/utilities";
 
-const spritesCanvas = <HTMLCanvasElement> document.getElementById("canvas");
-const spritesDrawingContext = spritesCanvas.getContext("2d");
+export default class Sprites {
+    private canvas: HTMLCanvasElement;
+    private drawingContext: CanvasRenderingContext2D;
 
-const ballPainter: IPainter = {
-    paint: (sprite, drawingContext) => {
+    constructor() {
+        this.canvas = <HTMLCanvasElement> document.getElementById("canvas");
+        this.drawingContext = this.canvas.getContext("2d");
 
-        const radius = sprite.dimension.width / 2;
-        const x = sprite.left + radius;
-        const y = sprite.top + radius;
-        drawingContext.save();
+        const ballPainter: IPainter = {
+            paint: (sprite, drawingContext) => {
 
-        drawingContext.beginPath();
-        drawingContext.arc(x, y, radius, 0, Math.PI * 2, false);
-        drawingContext.clip();
+                const radius = sprite.dimension.width / 2;
+                const x = sprite.left + radius;
+                const y = sprite.top + radius;
+                drawingContext.save();
 
-        drawingContext.shadowColor = "rgb(0,0,0)";
-        drawingContext.shadowOffsetX = -4;
-        drawingContext.shadowOffsetY = -4;
-        drawingContext.shadowBlur = 8;
+                drawingContext.beginPath();
+                drawingContext.arc(x, y, radius, 0, Math.PI * 2, false);
+                drawingContext.clip();
 
-        drawingContext.lineWidth = 2;
-        drawingContext.strokeStyle = "rgb(100, 100, 195)";
-        drawingContext.fillStyle = "rgba(30, 144, 255, .15)";
-        drawingContext.fill();
-        drawingContext.stroke();
+                drawingContext.shadowColor = "rgb(0,0,0)";
+                drawingContext.shadowOffsetX = -4;
+                drawingContext.shadowOffsetY = -4;
+                drawingContext.shadowBlur = 8;
 
-        drawingContext.restore();
+                drawingContext.lineWidth = 2;
+                drawingContext.strokeStyle = "rgb(100, 100, 195)";
+                drawingContext.fillStyle = "rgba(30, 144, 255, .15)";
+                drawingContext.fill();
+                drawingContext.stroke();
+
+                drawingContext.restore();
+            }
+        };
+
+        const ball = new Sprite("ball", {height: 75, width: 75}, ballPainter);
+
+        drawGrid(this.drawingContext, "lightgray", 10, 10);
+        ball.left = 160;
+        ball.top = 320;
+
+        ball.paint(this.drawingContext);
+
+        ball.left = 100;
+        ball.top = 20;
+
+        ball.paint(this.drawingContext);
     }
-};
-
-const ball = new Sprite("ball", {height: 75, width: 75}, ballPainter);
-
-drawGrid(spritesDrawingContext, "lightgray", 10, 10);
-ball.left = 160;
-ball.top = 320;
-
-ball.paint(spritesDrawingContext);
-
-ball.left = 100;
-ball.top = 20;
-
-ball.paint(spritesDrawingContext);
+}
