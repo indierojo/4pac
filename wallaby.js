255b6b2794524227fb7763702d70e04c63f1b8ab
Jon Bullock <jondooger@gmail.com>
2017-02-06 08:09:08 -0700

    Fix issue with wallaby.js after changes to tsconfig.

--- wallaby.js
+++ wallaby.js
@@ -1,12 +1,19 @@
-module.exports = function() {
+module.exports = function(wallaby) {
     return {
         'files': [
-            'src/**/*.ts'
+            'src/**/*.ts',
+            'src/*.ts'
         ], 'tests': [
-            'test/**/*Spec.ts'
+            'test/**/*Spec.ts',
+            'test/*Spec.ts'
         ],
         env: {
             type: 'node'
+        },
+        compilers: {
+            '**/*.ts': wallaby.compilers.typeScript({
+                module: 'commonjs',
+            })
         }
     };
 };
