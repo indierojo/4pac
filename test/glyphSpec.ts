b116b00cf38a3a3cb88c8674a7206b4e21f9101a
Jon Bullock <jondooger@gmail.com>
2017-02-06 08:20:05 -0700

    Simplify glyph spec.

--- test/glyphSpec.ts
+++ test/glyphSpec.ts
@@ -7,68 +7,41 @@ describe("Glyph", () => {
             const a = new Circle({x: 0, y: 0}, 3, "green");
             const b = new Circle({x: 0, y: 0}, 3, "blue");
             expect(a.collidesWith(b)).to.equal(true);
+            expect(b.collidesWith(a)).to.equal(true);
         });
 
         // A
         // B
-        it("should properly detect collisions of two objects overlapping on the top", () => {
+        it("should properly detect collisions of two objects overlapping vertically", () => {
             const a = new Circle({x: 0, y: -3}, 3, "green");
             const b = new Circle({x: 0, y: 0}, 3, "blue");
             expect(a.collidesWith(b)).to.equal(true);
+            expect(b.collidesWith(a)).to.equal(true);
         });
-        // B
-        // A
-        it("should properly detect collisions of two objects overlapping on the bottom", () => {
-            const a = new Circle({x: 0, y: 3}, 3, "green");
-            const b = new Circle({x: 0, y: 0}, 3, "blue");
-            expect(a.collidesWith(b)).to.equal(true);
-        });
+
         // AB
-        it("should properly detect collisions of two objects overlapping on the left", () => {
+        it("should properly detect collisions of two objects overlapping horizontally", () => {
             const a = new Circle({x: -3, y: 0}, 3, "green");
             const b = new Circle({x: 0, y: 0}, 3, "blue");
             expect(a.collidesWith(b)).to.equal(true);
-        });
-        // BA
-        it("should properly detect collisions of two objects overlapping on the left", () => {
-            const a = new Circle({x: 3, y: 0}, 3, "green");
-            const b = new Circle({x: 0, y: 0}, 3, "blue");
-            expect(a.collidesWith(b)).to.equal(true);
+            expect(b.collidesWith(a)).to.equal(true);
         });
     });
     describe("Collision detection, negative", () => {
         // A
         // B
-        it("should properly detect NO collisions of two objects vertically above", () => {
+        it("should properly detect NO collisions of two objects vertically", () => {
             const a = new Circle({x: 0, y: -6.01}, 3, "green");
             const b = new Circle({x: 0, y: 0}, 3, "blue");
             expect(a.collidesWith(b)).to.equal(false);
-        });
-        // B
-        // A
-        it("should properly detect NO collisions of two objects vertically below", () => {
-            const a = new Circle({x: 0, y: 6.01}, 3, "green");
-            const b = new Circle({x: 0, y: 0}, 3, "blue");
-            expect(a.collidesWith(b)).to.equal(false);
+            expect(b.collidesWith(a)).to.equal(false);
         });
         // AB
         it("should properly detect NO collisions of two objects horizontally left", () => {
             const a = new Circle({x: -6.01, y: 0}, 3, "green");
             const b = new Circle({x: 0, y: 0}, 3, "blue");
             expect(a.collidesWith(b)).to.equal(false);
-        });
-        // BA
-        it("should properly detect NO collisions of two objects horizontally right", () => {
-            const a = new Circle({x: 6.01, y: 0}, 3, "green");
-            const b = new Circle({x: 0, y: 0}, 3, "blue");
-            expect(a.collidesWith(b)).to.equal(false);
+            expect(b.collidesWith(a)).to.equal(false);
         });
     });
-
-    //   |
-    //  |||
-    // |||||
-    //  |||
-    //   |
-
 });
